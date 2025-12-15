856ee2e1a33712f36233019f98791d4cc38e4239
NehaThapa17 <105646225+NehaThapa17@users.noreply.github.com>
2025-12-15 10:33:14 +0400

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def sidebar():
     )
     
     st.sidebar.markdown("---")
-    st.sidebar.info("**Masters of AI in Business**\n\nData Visualization Assignment\n\n*NovaMart - Marketing Analytics Dashboard*")
+    st.sidebar.info("**Neha Thapa**\n\nMasters of AI in Business**\n\nData Visualization Assignment\n\n*NovaMart - Marketing Analytics Dashboard*")
     
     return page
 
@@ -617,4 +617,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
